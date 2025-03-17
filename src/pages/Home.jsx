9e314952e1ada7f094065661175f7e044a54338a
Sammy G <78850919+sam-gtz@users.noreply.github.com>
2025-03-16 19:46:26 -0700

    Adding search button

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,7 +6,22 @@ function Home() {
         { id: 2, title: "Sam Wiz", release_date: "2020" },
         { id: 3, title: "Whis Wiz", release_date: "2020" },
     ];
+
+    const handleSearch = () => {};
     return <div className="Home">
+        <div className="home">
+            <form onSubmit={handleSearch} className="search-form">
+                <input 
+                    type="text"
+                    placeholder="Search for dem movies..."
+                    className="search-input"
+                />
+                <button type="submit" className="search-button">
+                    PUSH
+                </button>
+
+            </form>
+        </div>
         <div className="movies-grid">
             { movies.map((m) => (
                 <MovieCard movie={m} key={m.id} /> 
