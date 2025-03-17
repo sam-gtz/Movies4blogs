341f0eba4698f8d95e1f6a17c820ba4af4c194d3
Sammy G <78850919+sam-gtz@users.noreply.github.com>
2025-03-16 21:00:09 -0700

    Added state to search bar in Home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,20 @@
 import MovieCard from "../components/MovieCard"
+import { useState } from "react"
 
 function Home() {
+
+    const [searchQuery, setSearchQuery] = useState("");
+
     const movies = [
         { id: 1, title: "John Wiz", release_date: "2020" },
         { id: 2, title: "Sam Wiz", release_date: "2020" },
         { id: 3, title: "Whis Wiz", release_date: "2020" },
     ];
 
-    const handleSearch = () => {};
+    const handleSearch = (e) => {
+        e.preventDefault()
+        alert(searchQuery)
+    };
     return <div className="Home">
         <div className="home">
             <form onSubmit={handleSearch} className="search-form">
@@ -15,6 +22,8 @@ function Home() {
                     type="text"
                     placeholder="Search for dem movies..."
                     className="search-input"
+                    value={searchQuery}
+                    onChange={(e) => setSearchQuery(e.target.value)}
                 />
                 <button type="submit" className="search-button">
                     PUSH
