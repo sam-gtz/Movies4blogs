da6fce8de565521bf7394f640c83068625451852
Sammy G <78850919+sam-gtz@users.noreply.github.com>
2025-03-18 23:08:37 -0700

    Added state to the home component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,30 @@
 import MovieCard from "../components/MovieCard"
-import { useState } from "react"
+import { useState, useEffect } from "react"
+import { searchMovies, getPopularMovies } from "../services/api"
 import "../css/Home.css"
 
 function Home() {
 
     const [searchQuery, setSearchQuery] = useState("");
+    const [movies, setMovies] = useState([]);
+    const [error, setError] = useState([]);
+    const [loading, setLoading] = useState(true);
 
-    const movies = [
-        { id: 1, title: "John Wiz", release_date: "2020" },
-        { id: 2, title: "Sam Wiz", release_date: "2020" },
-        { id: 3, title: "Whis Wiz", release_date: "2020" },
-    ];
+    useEffect(() => {
+        const loadPopularMovies = async () => {
+            try {
+                const popularMovies = await getPopularMovies()
+                setMovies(popularMovies)
+            } catch (err) {
+                console.log(err)
+                setError("Failed to load movies...")
+            } finally {
+                setLoading(false)
+            }
+        }
+
+        loadPopularMovies()
+    }, []) 
 
     const handleSearch = (e) => {
         e.preventDefault()
